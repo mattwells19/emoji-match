2539cc5360743c3e8d29da0a9cb339d2430e9589
Matt Wells <mattwells878@gmail.com>
2024-01-30 14:41:08 -0500

    fix timer for real

--- src/Timer.tsx
+++ src/Timer.tsx
@@ -1,15 +1,25 @@
-import { createSignal, Component, createEffect, onCleanup } from "solid-js";
+import {
+  createSignal,
+  Component,
+  createEffect,
+  onCleanup,
+  onMount,
+} from "solid-js";
 
 export const Timer: Component<{ gameOver: boolean }> = (props) => {
   const [value, setValue] = createSignal<number>(0);
   let timer = -1;
 
+  onMount(() => {
+    if (!props.gameOver) {
+      setValue(0);
+      timer = setInterval(() => setValue(value() + 1), 1000);
+    }
+  });
+
   createEffect(() => {
     if (props.gameOver) {
       clearInterval(timer);
-    } else {
-      setValue(0);
-      timer = setInterval(() => setValue(value() + 1), 1000);
     }
   });
 
